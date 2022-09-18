5dfb929a9426427006cfbccae6e3a008e41ea66b
HassanatB-O <hassanatsubomi@gmail.com>
2022-09-18 13:19:47 +0100

    Updates

---
+++ precache-manifest.8b7ef06537cf96b496a9b7af075ab3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d0206bc24b19c4bd8e",
+    "url": "/react-validation-form/static/css/main.2e90ce46.chunk.css"
+  },
+  {
+    "revision": "23d0206bc24b19c4bd8e",
+    "url": "/react-validation-form/static/js/main.23d0206b.chunk.js"
+  },
+  {
+    "revision": "391e9cd3c025acf33468",
+    "url": "/react-validation-form/static/js/1.391e9cd3.chunk.js"
+  },
+  {
+    "revision": "577f4131f44be33796b0",
+    "url": "/react-validation-form/static/js/2.577f4131.chunk.js"
+  },
+  {
+    "revision": "db97775bc00729358bb3",
+    "url": "/react-validation-form/static/js/runtime~main.db97775b.js"
+  },
+  {
+    "revision": "aa6adbe0dccf6e85dca306cfb1c52470",
+    "url": "/react-validation-form/index.html"
+  }
+];
